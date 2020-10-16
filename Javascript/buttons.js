274a79041b7685ea6bef0c5b5c98f3ec90671510
minhazulmoon-rgb <69197671+minhazulmoon-rgb@users.noreply.github.com>
2020-10-16 21:40:59 +0600

    Add files via upload

---
+++ Javascript/buttons.js
@@ -0,0 +1,23 @@
+//  learn more button in working box starts
+function toggleHide() {
+    let btn = document.getElementById('btn');
+    let para = document.getElementById('para');
+    if (para.style.display != 'none') {
+        para.style.display = 'none';
+    }
+    else {
+        para.style.display = 'block';
+    }
+}
+//  learn more button in working box starts
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, labore soluta consectetur nulla aliquam assumenda adipisci, nam obcaecati eveniet eius quidem iusto sequi quam delectus dolorum itaque, a provident quod iure dignissimos odio error? Molestias in pariatur nesciunt assumenda a suscipit, corporis temporibus ex modi aspernatur quas architecto alias perferendis, porro nulla harum. Hic, expedita. Pariatur recusandae, praesentium quia voluptas natus expedita maiores labore sint esse incidunt quibusdam accusantium velit iste? Repellat, iste deleniti. Corporis." ;
+}
+
+
+// policy button starts
+function myFunction2() {
+    document.getElementById("demo2").innerHTML = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, labore soluta consectetur nulla aliquam assumenda adipisci, nam obcaecati eveniet eius quidem iusto sequi quam delectus dolorum itaque, a provident quod iure dignissimos odio error? Molestias in pariatur nesciunt assumenda a suscipit, corporis temporibus ex modi aspernatur quas architecto alias perferendis, porro nulla harum. Hic, expedita. Pariatur recusandae, praesentium quia voluptas natus expedita maiores labore sint esse incidunt quibusdam accusantium velit iste? Repellat, iste deleniti. Corporis. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui voluptates aperiam ipsa non enim placeat recusandae laborum eius nemo? Ipsam nulla quidem autem sequi tempore dignissimos aut commodi libero possimus esse quam dolor porro, eum rerum atque qui consectetur, eveniet voluptate vel! Omnis laboriosam, exercitationem dolorem, maxime eveniet sit eligendi molestiae recusandae earum, impedit voluptatibus." ;
+}
+
+// policy button endss
